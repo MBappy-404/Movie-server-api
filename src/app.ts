f8c32b6a9aee516aed1fb7c42902b4be609a4ba2
sopnilali <sopnilali24@gmail.com>
2025-04-30 12:03:52 +0600

    user

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ import express, { NextFunction, Request, Response }  from "express";
 import cors from "cors";
 import  HttpStatus  from "http-status";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import router from "./app/routes";
 
 // middlewares 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+
+app.use('/api', router)
 
 app.get("/", (req, res) => {
   res.send("Movie server is running!!");
--- src/app/helper/sendResponse.ts
+++ src/app/helper/sendResponse.ts
@@ -3,13 +3,13 @@ import { Response } from "express";
 export const sendResponse = <T>(
   res: Response,
   data: {
-    status: number;
+    statusCode: number;
     success: boolean;
     message: string;
     data?: T | null | undefined;
   }
 ) => {
-  res.status(data?.status).json({
+  res.status(data?.statusCode).json({
     success: data.success,
     message: data?.message,
     data: data?.data || null || undefined,
