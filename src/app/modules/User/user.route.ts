7ab0b58f01945b8d3e819229f5bbf05fefcbd51b
sopnilali <sopnilali24@gmail.com>
2025-04-30 13:29:49 +0600

    zod validation added

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -4,14 +4,16 @@ import express from 'express'
 import { UserController } from './user.controller'
 import validateRequest from '../../middleware/validateRequest'
 import { UserValidation } from './user.validation'
+import auth from '../../middleware/auth'
+import { UserRole } from '@prisma/client'
 
 const router = express.Router()
 
 router.post('/register', validateRequest(UserValidation.createUserValidation), UserController.RegisterUser)
-router.get('/', UserController.getAllUserData)
+router.get('/', auth(UserRole.ADMIN), UserController.getAllUserData)
 router.get('/:id', UserController.getUserById)
-router.patch('/:id', UserController.UpdateUser)
-router.delete('/:id',UserController.DeleteUser)
+router.patch('/:id',  auth(UserRole.ADMIN, UserRole.USER), UserController.UpdateUser)
+router.delete('/:id', auth(UserRole.ADMIN), UserController.DeleteUser)
 
 
 export const UserRoutes = router
