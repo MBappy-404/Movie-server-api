bb9275de6457c75b9263502b8081869867350c05
thkiaAdiba <athkiaadiba@gmail.com>
2025-04-30 20:18:50 +0600

    added content model

--- prisma/migrations/20250430064202_genre/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Genre" (
-    "id" TEXT NOT NULL,
-    "genreName" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Genre_pkey" PRIMARY KEY ("id")
-);
